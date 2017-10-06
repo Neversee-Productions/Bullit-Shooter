d6860be91ad33d1ba5796764115b6dad435efc47
rafael-plugge <tini.shadow@email.com>
2017-10-06 16:43:54 +0100

    Created initial Solution File

---
+++ Project/src/main.cpp
@@ -0,0 +1,9 @@
+/// <summary>
+/// Main entry point for the application
+/// </summary>
+/// <param name="argc">number of arguments passed into the application</param>
+/// <param name="argv">c style string of the arguments passed into the application</param>
+int main(int argc, char** argv)
+{
+	
+}
