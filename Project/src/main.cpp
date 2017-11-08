a318497a0138716de7d03c4e3cc2fb44a4e9e082
rafael-plugge <tini.shadow@email.com>
2017-11-08 09:46:38 +0000

    This branch will contain all of my work for sprint 2

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -54,7 +54,7 @@
 /// @file
 /// </summary>
 /// <param name="argc">number of arguments passed into the application.</param>
-/// <param name="argv">c style string of the arguments passed into the application.</param>
+/// <param name="argv">c style string of the arguments passed into the application</param>
 int main(int argc, char** argv)
 {
 #ifdef NDEBUG
