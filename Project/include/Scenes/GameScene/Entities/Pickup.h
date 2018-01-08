990af7d530721d8ff601feda6e90e39dbfc754c9
sybek96 <sybek96@interia.pl>
2018-01-08 16:57:01 +0000

    Started the upgrading feature by adding

---
+++ Project/include/Scenes/GameScene/Entities/Pickup.h
@@ -0,0 +1,35 @@
+﻿#ifndef PICKUP_H
+#define PICKUP_H
+
+#include <string>
+#include <memory>
+#include "SFML\Graphics.hpp"
+
+class Pickup
+{
+
+public:
+	/// <summary>
+	/// @author Sebastian Kruzel
+	/// @brief Define resources for the pickup
+	/// 
+	/// 
+	/// </summary>
+	struct Resources
+	{
+		struct IndividualPickup
+		{
+			std::string m_id = "";
+			std::shared_ptr<sf::Texture> m_texture;
+			sf::Vector2f m_size;
+		};
+
+		std::vector<IndividualPickup> m_pickups;
+	};
+
+private:
+	std::shared_ptr<sf::Texture> m_texture;
+
+};
+
+#endif // !PICKUP_H
